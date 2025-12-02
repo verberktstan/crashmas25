54bcf721d406de182ff0fa0f57d910aff02aab3c
Stan Verberkt <stanv@viduet.eu>
2025-12-02 09:58:48 +0100

    Day 1 - factor out rotation operator intead

--- src/crashmas25/day1.clj
+++ src/crashmas25/day1.clj
@@ -9,24 +9,20 @@
   [s]
   (when-let [direction (and s (some-> s first {\L :left \R :right}))]
     (when-let [steps (some->> s rest (apply str) edn/read-string)]
-      {:rotation/direction direction
-       :rotation/steps     steps})))
+      {:rotation/steps     steps
+       :rotation/operator (get {:left -} direction +)})))
 
 (test/deftest parse-rotation-test
-  (test/is (= {:rotation/direction :left :rotation/steps 42}
+  (test/is (= {:rotation/steps 42 :rotation/operator -}
               (parse-rotation "L42"))))
 
 (defn- read-input
   "Reads all the lines from `filename` and returns the lines parsed by `parser`."
   ([filename] (read-input filename nil))
   ([filename parser]
-   (with-open [reader (io/reader filename)]
+   (with-open [reader (io/reader (str "resources/" filename))]
      (cond->> (-> reader line-seq doall)
        parser (map parser)))))
-
-(comment
-  ;; Test read-input
-  (read-input "resources/day1test.txt"))
 
 (defn- wrapped
   "Returns a function that does the same as op (- or +) and wraps it between zero
@@ -56,9 +52,8 @@
 
 (defn- do-rotation
   "Given a dial and a rotation direction/step, return the dial after rotating."
-  [dial {:rotation/keys [direction steps]}]
-  (let [operator (if (= direction :left) - +)
-        steps    (mod steps 100)
+  [dial {:rotation/keys [steps operator]}]
+  (let [steps    (mod steps 100)
         dial     (update dial :dial/at (wrapped operator 100) steps)]
     (cond-> dial
       (some-> dial :dial/at zero?)
@@ -67,30 +62,28 @@
 (test/deftest do-rotation-test
   (let [dial {:dial/at 50 :dial/land-on-zero-count 0}]
     (test/are [input output] (= output (do-rotation dial input))
-      {:rotation/direction :left :rotation/steps 1}
+      {:rotation/operator - :rotation/steps 1}
       (assoc dial :dial/at 49)
 
-      {:rotation/direction :right :rotation/steps 1}
+      {:rotation/operator + :rotation/steps 1}
       (assoc dial :dial/at 51)
 
-      {:rotation/direction :left :rotation/steps 52}
+      {:rotation/operator - :rotation/steps 52}
       (assoc dial :dial/at 98)
 
-      {:rotation/direction :right :rotation/steps 53}
+      {:rotation/operator + :rotation/steps 53}
       (assoc dial :dial/at 3)
 
-      {:rotation/direction :left :rotation/steps 50}
+      {:rotation/operator - :rotation/steps 50}
       {:dial/at 0 :dial/land-on-zero-count 1})))
 
 (defn- count-zero-crossings
-  [dial {:rotation/keys [direction steps]}]
-  (let [operator (if (= direction :left) - +)
-        mod-op (comp #(mod % 100) operator)
-        coll (rest
-              (reductions mod-op
-                          (:dial/at dial)
-                          (repeat steps 1)))
-        zc-count (->> coll (filter zero?) count)]
+  [dial {:rotation/keys [steps operator]}]
+  (let [mod-op   (comp #(mod % 100) operator)
+        coll     (reductions mod-op
+                             (:dial/at dial)
+                             (repeat steps 1))
+        zc-count (->> coll rest (filter zero?) count)]
     (-> dial
         (update :dial/at mod-op steps)
         (update :dial/zc-count + zc-count))))
@@ -110,15 +103,15 @@
                 (read-input parse-rotation)
                 (follow-rotations count-zero-crossings)
                 :dial/zc-count))]
-    (test/is (-> "resources/day1test.txt"
+    (test/is (-> "day1/test.txt"
                  day1part1
                  (= 3)))
-    (test/is (-> "resources/day1input.txt"
+    (test/is (-> "day1/input.txt"
                  day1part1
                  (= 1011)))
-    (test/is (-> "resources/day1test.txt"
+    (test/is (-> "day1/test.txt"
                  day1part2
                  (= 6)))
-    (test/is (-> "resources/day1input.txt"
+    (test/is (-> "day1/input.txt"
                  day1part2
                  (= 5937)))))
