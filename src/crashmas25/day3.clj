f398958545d72e4a61624bc30edb0c4e17a83683
Stan Verberkt <stanv@viduet.eu>
2025-12-04 13:06:27 +0100

    Day 3 - Refactor filtering of batteries

--- src/crashmas25/day3.clj
+++ src/crashmas25/day3.clj
@@ -7,13 +7,22 @@
   (memoize (fn [index char]
              {:battery/index index :battery/value (-> char str edn/read-string)})))
 
+(defn- filter-batteries
+  ([battery batteries]
+   (filter-batteries 1 battery batteries))
+  ([n {:battery/keys [index]} batteries]
+   (into [] (comp (filter #(> (:battery/index %) index)) (take n)) batteries)))
+
 (defn- joltage-for-combo
   "Return the joltage for a combination of batteries."
-  [sorted-batteries s {:battery/keys [index value]}]
-  (let [coll      (filter #(> (:battery/index %) index) sorted-batteries) ;; TODO: transduce here with take 1?
-        new-value (str value (some-> coll first :battery/value))]
-    (cond-> s
-      (seq coll) (conj (edn/read-string new-value)))))
+  ([sorted-batteries s battery]
+   (joltage-for-combo 1 sorted-batteries s battery))
+  ([n sorted-batteries s {:battery/keys [value] :as battery}]
+   (let [coll      (filter-batteries n battery sorted-batteries)
+        ;; TODO: For part 2 the new-value should consist of 12 digits instead of 2. w00t
+         new-value (->> coll (keep :battery/value) (apply str value))]
+     (cond-> s
+       (seq coll) (conj (edn/read-string new-value))))))
 
 (defn- largest-joltage
   "Return the largest-joltage for a bank of batteries"
@@ -21,13 +30,13 @@
   (let [sorted-batteries (sort-by :battery/value > battery-bank)
         get-joltage      (partial joltage-for-combo sorted-batteries)
         combos           (reduce get-joltage #{} sorted-batteries)]
-    (reduce max combos)))
+    (some->> combos seq (reduce max))))
 
 (defn- day3part1 [filename]
   (->> filename
        read-input
        (map (partial map-indexed indexed-char))
-       (map largest-joltage)
+       (keep largest-joltage)
        (reduce + 0)
        time))
 
